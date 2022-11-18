179581440cad9128a758f23f1749679b3934e1f9
Jaroslav Klapálek <klapajar@fel.cvut.cz>
2022-11-18 12:44:34 +0100

    compile: Add three new options

--- vxcli/command/compile/main.py
+++ vxcli/command/compile/main.py
@@ -9,10 +9,13 @@
 import argparse
 import subprocess
 import os
+import sys
 from vxcli import IPopen
 
 # Global variables
 HELP = "wr-cc compiler"
+DKM_FLAGS = ["-dkm", "-g", "-o", "binary.out"]
+RTP_FLAGS = ["-rtp", "-g", "-static", "-o", "binary.vxe"]
 
 
 ######################
@@ -38,12 +41,35 @@ $ vxcli compile -g -rtp <source files> -static -o <outfile>
         default = os.environ.get("WIND_CC_SYSROOT", os.environ.get("WIND_BASE") + "/samples/prebuilt_projects/vsb_vxsim_linux"),
     )
 
+    subparser.add_argument("files",
+        nargs = "+",
+        help = "source files to be compiled",
+        metavar = "<source file>",
+    )
+
+    subparser.add_argument("--dkm",
+        dest = "dkm",
+        help = "use usual flags for compiling the source files as DKM (set to: %s)" % " ".join(DKM_FLAGS),
+        action = "store_true",
+    )
+
+    subparser.add_argument("--rtp",
+        dest = "rtp",
+        help = "use usual flags for compiling the source files as RTP (set to: %s)" % " ".join(RTP_FLAGS),
+        action = "store_true",
+    )
+
 
 ######################
 # run
 ######################
 
-def run(vsb, other_args):
+def run(files, dkm, rtp, vsb, other_args):
+
+    if dkm and rtp:
+        print ("Unable to use two conflicting options '--dkm' and '--rtp'.", file = sys.stderr)
+        exit (1)
+
     os.environ["WIND_CC_SYSROOT"] = vsb
 
     _ip = IPopen.IPopen()
@@ -51,5 +77,8 @@ def run(vsb, other_args):
     _ip.run([
             os.environ["WIND_BASE"] + "/host/x86_64-linux/bin/wr-cc",
         ]
-        + [ "--help" if "-help" else arg for arg in other_args ] # Their help is broken.
+        + (DKM_FLAGS if dkm else [])
+        + (RTP_FLAGS if rtp else [])
+        + files
+        + [ "--help" if arg == "-help" else arg for arg in other_args ] # Their help is broken.
     )
