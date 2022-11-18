59e12e2d960fe06e2a325691106a3ff774534b0b
Jaroslav Klapálek <klapajar@fel.cvut.cz>
2022-11-18 12:06:01 +0100

    debug: Add new command

---
+++ vxcli/command/debug/main.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# debug
+# Attaches a debugger to a target device.
+
+######################
+# Imports & Globals
+######################
+
+import argparse
+import subprocess
+import os
+from vxcli import IPopen
+
+# Global variables
+HELP = "Device debugger"
+
+os.environ["WIND_WB_HOSTTOOLS"] = os.environ["WIND_HOME"] + "/workbench-4/22.06/x86_64-linux2"
+
+
+######################
+# Init Parser
+######################
+
+def init_parser(subparser):
+    subparser.add_argument("--host",
+        metavar = "<hostname>",
+        dest = "host",
+        help = "IP address or hostname of the target device (default: localhost)",
+        type = str,
+        default = "localhost",
+    )
+
+    subparser.add_argument("--port",
+        metavar = "<port>",
+        dest = "port",
+        help = "port used for communication with the device (default: 1534)",
+        type = int,
+        default = 1534,
+    )
+
+    subparser.add_argument("--kernel",
+        metavar = "<file name>",
+        dest = "kernel",
+        help = "VxWorks 7 core file to load (default: vxWorks or os.environ[\"KERNEL_IMAGE\"] if set)",
+        type = str,
+        default = os.environ.get("KERNEL_IMAGE", "vxWorks"),
+    )
+
+
+######################
+# run
+######################
+
+def run(host, port, kernel, other_args):
+    _ip = IPopen.IPopen()
+
+    _ip.run([
+            os.environ["WIND_HOME"] + "/workbench-4/22.06/x86_64-linux2/bin/wrpython",
+            "-u",
+            os.environ["WIND_HOME"] + "/workbench-4/22.06/x86_64-linux2/lib/python/windriver/shells/WrDbg.py"
+        ]
+        + [
+            "--eval-command", "target connect vxworks7:%s:%d -kernel %s" % (host, port, kernel),
+        ]
+        + other_args
+    )
